054813089bf124296b35720560f2fd60023846d5
MariaMilkova <116704409+MariaMilkova@users.noreply.github.com>
2023-03-19 01:43:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int start = 0;
+    int end = size - 1;
+    while (start <= end) {
+        int mean = (start + end) / 2;
+        if (value == arr[mean]) {
+            int flag = mean + 1;
+            while (value == arr[mean]) {
+                mean--;
+                count++;
+            }
+            while (value == arr[flag]) {
+                flag++;
+                count++;
+            }
+            return count;
+        } else if (value < arr[mean]) {
+            end = mean - 1;
+        } else if (value > arr[mean]) {
+            start = mean + 1;
+        }
+    }
+    return 0;
 }
